3de7cc63736090e315e2565b67014f37ececd62e
Yuhao <yuhao.chen2000@icloud.com>
2022-01-22 00:37:19 -0800

    pink can now find anchor point base on tag and distance

--- Fragments Of You/Assets/Scripts/PinkMovement.cs
+++ Fragments Of You/Assets/Scripts/PinkMovement.cs
@@ -7,19 +7,21 @@ public class PinkMovement : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D coll;
     private SpriteRenderer sprite;
+    private SpringJoint2D arm;
 
     [SerializeField] private LayerMask jumpableGround;
 
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
-
+    [SerializeField] private float anchorableDis = 14f;
     private void Start()
     {
         // get component
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
+        arm = GetComponent<SpringJoint2D>();
 
         // freeze rotation
         rb.freezeRotation = true;
@@ -66,5 +68,26 @@ public class PinkMovement : MonoBehaviour
         } else if (rb.velocity.x > 0.1f) {
             sprite.flipX = false;
         }
+    }
+
+    // Find Valid Anchor within anchorableDis
+    private GameObject FindValidAnchor() 
+    {
+        GameObject[] anchors;
+        anchors = GameObject.FindGameObjectsWithTag("Anchor");
+        GameObject valid = null;
+        float distance = anchorableDis;
+        Vector3 position = transform.position;
+        foreach (GameObject an in anchors)
+        {
+            Vector3 diff = an.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                valid = an;
+                distance = curDistance;
+            }
+        }
+        return valid;
     }
 }
