27fa3ef11b6657c2dca9149b8bbfd2fe209b36f2
Yuhao <yuhao.chen2000@icloud.com>
2022-01-22 02:06:25 -0800

    additional clean up

--- Fragments Of You/Assets/Scripts/PinkMovement.cs
+++ Fragments Of You/Assets/Scripts/PinkMovement.cs
@@ -7,22 +7,29 @@ public class PinkMovement : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D coll;
     private SpriteRenderer sprite;
+    private SpringJoint2D arm;
 
     [SerializeField] private LayerMask jumpableGround;
 
     private float dirX = 0f;
+    private bool isAnchored = false;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
-
+    [SerializeField] private float grappleJumpForce = 7f;
+    [SerializeField] private float anchorableDis = 14f;
     private void Start()
     {
         // get component
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
+        arm = GetComponent<SpringJoint2D>();
 
         // freeze rotation
         rb.freezeRotation = true;
+
+        arm.enabled = false;
+        isAnchored = false;
     }
 
     private void Update()
@@ -30,25 +37,56 @@ public class PinkMovement : MonoBehaviour
         Move();
         FlipPlayer();
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            if(IsGrounded())
+            {
+                Jump();
+            }
+            else if(IsAnyValidAnchor() && !isAnchored)
+            {
+                Grapple();
+            } else if(isAnchored)
+            {
+                GrappleOff();
+            }
         }
+
+        if(IsAnyValidAnchor())
+        {
+            arm.connectedBody = FindValidAnchor().GetComponent<Rigidbody2D>();
+        }
+        AnchorRadar();
     }
     
+    private void FixedUpdate() 
+    {
+        
+    }
     private void Move() 
     {
         // get input
         dirX = Input.GetAxisRaw("Horizontal");
         // modify velocity based on input
-        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        rb.AddForce(Vector2.right * dirX * moveSpeed, ForceMode2D.Force);
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
+    // Grapple
+    private void Grapple() {
+        arm.enabled = true;
+        isAnchored = true;
+    }
+    // Disengage Grapple
+    private void GrappleOff() {
+        arm.enabled = false;
+        isAnchored = false;
+        rb.AddForce(Vector2.up * grappleJumpForce, ForceMode2D.Impulse);
+    }
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
@@ -61,6 +99,59 @@ public class PinkMovement : MonoBehaviour
             sprite.flipX = true;
         } else if (rb.velocity.x > 0.1f) {
             sprite.flipX = false;
+        }
+    }
+    
+    // Find Valid Anchor within anchorableDis
+    private GameObject FindValidAnchor() 
+    {
+        GameObject[] anchors;
+        anchors = GameObject.FindGameObjectsWithTag("Anchor");
+        GameObject valid = null;
+        float distance = anchorableDis;
+        Vector3 position = transform.position;
+        foreach (GameObject an in anchors)
+        {
+            Vector3 diff = an.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                valid = an;
+                distance = curDistance;
+            }
+        }
+        return valid;
+    }
+    // Return true if an anchor within range
+    private bool IsAnyValidAnchor() 
+    {
+        GameObject[] anchors;
+        anchors = GameObject.FindGameObjectsWithTag("Anchor");
+        float distance = anchorableDis;
+        Vector3 position = transform.position;
+        foreach (GameObject an in anchors)
+        {
+            Vector3 diff = an.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private void AnchorRadar()
+    {
+        GameObject an = FindValidAnchor();
+        if(IsAnyValidAnchor()) {
+            if(isAnchored) {
+                Debug.DrawLine(transform.position, an.transform.position, Color.green);
+            } else 
+            {
+                Debug.DrawLine(transform.position, an.transform.position, Color.red);
+            }
+            
         }
     }
 }
