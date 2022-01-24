b3a2a4fbd9c65da4949a7c9b558103a59d85c85c
David Mayfield <davidmayfield1818@yahoo.com>
2022-01-24 15:45:11 -0800

    Added states for ability restriction, added some organization, added a basic respawn example

--- Fragments Of You/Assets/Scripts/PinkMovement.cs
+++ Fragments Of You/Assets/Scripts/PinkMovement.cs
@@ -19,6 +19,10 @@ public class PinkMovement : MonoBehaviour
     [SerializeField] private float jumpForce = 14f;
     [SerializeField] private float grappleJumpForce = 7f;
     [SerializeField] private float anchorableDis = 14f;
+
+    private bool hasArms = true;
+    private bool hasLegs = true;
+
     private void Start()
     {
         // get component
@@ -32,21 +36,27 @@ public class PinkMovement : MonoBehaviour
 
         arm.enabled = false;
         isAnchored = false;
+
+        // set respawn to starting position
+        respawnPoint = this.transform.position;
     }
 
 
     private void Update()
     {
+        // basic left/right movement
+        // player should always have this
         Move();
         FlipPlayer();
-
+        
+        // start of abilities that can be lost
         if (Input.GetButtonDown("Jump"))
         {
-            if(IsGrounded())
+            if(IsGrounded() && hasLegs)
             {
                 Jump();
             }
-            else if(IsAnyValidAnchor() && !isAnchored)
+            else if(IsAnyValidAnchor() && !isAnchored && hasArms)
             {
                 Grapple();
             } else if(isAnchored)
@@ -60,6 +70,12 @@ public class PinkMovement : MonoBehaviour
             arm.connectedBody = FindValidAnchor().GetComponent<Rigidbody2D>();
         }
         AnchorRadar();
+
+        // if player falls off level respawn
+        if(this.transform.position.y < -8)
+        {
+            respawnPlayer();
+        }
     }
     
     private void FixedUpdate() 
@@ -67,6 +83,7 @@ public class PinkMovement : MonoBehaviour
         
     }
 
+    // Movement functions start -------------------------------------------------------------------
     private void Move() 
     {
         // get input
@@ -80,6 +97,13 @@ public class PinkMovement : MonoBehaviour
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+    // Movement functions end ---------------------------------------------------------------------
+
+    // Grapple functions start --------------------------------------------------------------------
     // Grapple
     private void Grapple() {
         arm.enabled = true;
@@ -93,41 +117,6 @@ public class PinkMovement : MonoBehaviour
         rb.AddForce(Vector2.up * grappleJumpForce, ForceMode2D.Impulse);
     }
 
-    private bool IsGrounded()
-    {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
-    }
-
-    // sets repawnpoint taking vector 2 as input
-    // vector 3 can be converted to vector 2 implicitly
-    // Note: not set by default.
-    public void setRespawn(Vector2 newPoint)
-    {
-        respawnPoint = newPoint;
-    }
-
-    // gets repawnpoint returning vector 2
-    public Vector2 getRespawnPoint()
-    {
-        return respawnPoint;
-    }
-
-    // teleports player back to respawnpoint and corrects any orientation
-    public void respawnPlayer()
-    {
-        this.transform.SetPositionAndRotation(respawnPoint,new Quaternion(0,0,0,0));
-    }
-
-    private void FlipPlayer()
-    {
-        if (rb.velocity.x < -0.1f)
-        {
-            sprite.flipX = true;
-        } else if (rb.velocity.x > 0.1f) {
-            sprite.flipX = false;
-        }
-    }
-    
     // Find Valid Anchor within anchorableDis
     private GameObject FindValidAnchor() 
     {
@@ -180,4 +169,58 @@ public class PinkMovement : MonoBehaviour
             
         }
     }
+    // Grapple functions end ----------------------------------------------------------------------
+
+    // Respawn functions start --------------------------------------------------------------------
+    // sets repawnpoint taking vector 2 as input
+    // vector 3 can be converted to vector 2 implicitly
+    // Note: not set by default.
+    public void setRespawn(Vector2 newPoint)
+    {
+        respawnPoint = newPoint;
+    }
+
+    // gets repawnpoint returning vector 2
+    public Vector2 getRespawnPoint()
+    {
+        return respawnPoint;
+    }
+
+    // teleports player back to respawnpoint and corrects any orientation
+    public void respawnPlayer()
+    {
+        this.transform.SetPositionAndRotation(respawnPoint,new Quaternion(0,0,0,0));
+    }
+    // Respawn functions end ----------------------------------------------------------------------
+
+    // State functions start ----------------------------------------------------------------------
+    // function to lose arms
+    public void loseArms()
+    {
+        hasArms = false;
+        // preform sprite switch here
+
+    }
+
+    // function to lose legs
+    public void loseLegs()
+    {
+        hasLegs = false;
+        // preform sprite switch here
+
+    }
+    // State functions end ------------------------------------------------------------------------
+
+    // Other helper functions start ---------------------------------------------------------------
+    // flips player sprit to maintain correct orientation.
+    private void FlipPlayer()
+    {
+        if (rb.velocity.x < -0.1f)
+        {
+            sprite.flipX = true;
+        } else if (rb.velocity.x > 0.1f) {
+            sprite.flipX = false;
+        }
+    }
+    
 }
