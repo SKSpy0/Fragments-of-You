d9da3df006502baa7b9c172d18ba0bc8bed8b39c
Yuhao <yuhao.chen2000@icloud.com>
2022-01-25 17:01:16 -0800

    Grapple auto aim complete

--- Fragments Of You/Assets/Scripts/PinkGrapple.cs
+++ Fragments Of You/Assets/Scripts/PinkGrapple.cs
@@ -8,7 +8,7 @@ public class PinkGrapple : MonoBehaviour
     private BoxCollider2D coll;
     private SpringJoint2D arm;
     private Animator animator;
-    private GameObject arms;
+    public GameObject arms;
     private bool isAnchored = false;
     public bool hasArms = true;
     public bool advanceGrapple = true;
@@ -50,12 +50,22 @@ public class PinkGrapple : MonoBehaviour
             {
                 arm.connectedBody = FindValidAnchor().GetComponent<Rigidbody2D>();
             }
-            AnchorRadar();
         }
         else
         {
-            
+            if (Input.GetButtonDown("Jump"))
+            {
+                if(!IsGrounded() && IsAnyValidAnchor() && !isAnchored && hasArms)
+                {
+                    ShootArms();
+                } 
+                else if(isAnchored)
+                {
+                    ReleaseArms();
+                }
+            }
         }
+        AnchorRadar();
         
     }
 
@@ -78,6 +88,25 @@ public class PinkGrapple : MonoBehaviour
         arm.enabled = false;
         isAnchored = false;
         rb.AddForce(Vector2.up * grappleJumpForce, ForceMode2D.Impulse);
+    }
+    private void ShootArms()
+    {
+        arm.connectedBody = arms.GetComponent<Rigidbody2D>();
+        isAnchored = true;
+        animator.SetBool("isJumping", true);
+        Vector3 target = FindValidAnchor().transform.position;
+        Quaternion targetRotation = arms.GetComponent<ArmsScript>().GetRotation(target);
+        arms.GetComponent<ArmsScript>().SetTarget(target);
+        arms.GetComponent<ArmsScript>().SetRotation(targetRotation);
+        arms.GetComponent<ArmsScript>().Fire();
+        arm.enabled = true;
+    }
+    private void ReleaseArms()
+    {
+        isAnchored = false;
+        arm.enabled = false;
+        rb.AddForce(Vector2.up * grappleJumpForce, ForceMode2D.Impulse);
+        arms.GetComponent<ArmsScript>().Reset();
     }
     private GameObject FindValidAnchor() 
     {
@@ -122,7 +151,13 @@ public class PinkGrapple : MonoBehaviour
         GameObject an = FindValidAnchor();
         if(IsAnyValidAnchor()) {
             if(isAnchored) {
-                Debug.DrawLine(transform.position, an.transform.position, Color.green);
+                if(advanceGrapple) {
+                    Debug.DrawLine(transform.position, arms.transform.position, Color.yellow);
+                }
+                else
+                {
+                    Debug.DrawLine(transform.position, an.transform.position, Color.green);
+                }
             } else 
             {
                 Debug.DrawLine(transform.position, an.transform.position, Color.red);
