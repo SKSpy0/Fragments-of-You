83530fb83d762a905810f3545fa9483d8ad524a9
Yuhao <yuhao.chen2000@icloud.com>
2022-02-19 19:21:18 -0800

    Z1M2 mostly, block are now pickup-able (very buggy)
    # Conflicts:
    #	Fragments Of You/Assets/Scenes/Z1M2.unity
    #	Fragments Of You/UserSettings/EditorUserSettings.asset
    merge conflict

--- Fragments Of You/Assets/Scripts/Boxes/BoxScript.cs
+++ Fragments Of You/Assets/Scripts/Boxes/BoxScript.cs
@@ -5,16 +5,60 @@ using UnityEngine;
 public class BoxScript : MonoBehaviour
 {
     private Vector2 spawnPoint;
+    private bool isPickup;
+    private bool couldPickup;
+    private GameObject[] players;
+    private GameObject player;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer playerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject p in players) {
+            player = p;
+        }
+
+        rb = GetComponent<Rigidbody2D>();
+        playerSprite = player.GetComponent<SpriteRenderer>();
         spawnPoint = this.transform.position;
+        isPickup = false;
+        couldPickup = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyUp(KeyCode.F))
+        {
+            if(isPickup) {
+                isPickup = false;
+                couldPickup = false;
+                rb.simulated  = true;
+
+                if(playerSprite.flipX == false)
+                {
+                    rb.AddForce(Vector2.right * 2f, ForceMode2D.Impulse);
+                    rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+                }
+                else
+                {
+                    rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+                    rb.AddForce(Vector2.left * 2f, ForceMode2D.Impulse);
+                }
+            } else {
+                isPickup = true;
+            }
+            
+        }
+        if (couldPickup == true && isPickup)
+        { 
+            Vector2 target = new Vector2(player.transform.position.x, player.transform.position.y + 1f);
+            this.transform.position = target;
+            rb.simulated  = false;
+        }
     }
 
     public void respawn()
@@ -24,6 +68,11 @@ public class BoxScript : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D other)
     {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Enter!");
+            couldPickup = true;
+        }
         // if interaction is below then do the thing else don't
         if(other.transform.position.y < this.transform.position.y)
         {
@@ -42,6 +91,7 @@ public class BoxScript : MonoBehaviour
                     this.gameObject.transform.parent = other.gameObject.transform;
                 }
             }
+            
         }
         
     }
@@ -50,6 +100,14 @@ public class BoxScript : MonoBehaviour
 
     public void OnTriggerExit2D(Collider2D other)
     {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Exist!");
+            if(!isPickup)
+            {
+                couldPickup = false;
+            }
+        }
         //Debug.Log("BoxLeave: " + other.tag);
         // if interaction is below then do the thing else don't
         // When box leave moving platform, move freely
