74ee348d5776acd1e3a3ea81b6b87f202bca488a
Yufeng Xie <yxie54@ucsc.edu>
2022-04-17 23:14:13 +0800

    Z1M3 Draft Done

--- Fragments Of You/Assets/Scripts/Platforms/jumpPad.cs
+++ Fragments Of You/Assets/Scripts/Platforms/jumpPad.cs
@@ -6,6 +6,8 @@ public class jumpPad : MonoBehaviour
 {
     public float bounce = 10f;
     public AudioSource JumpPadSFX;
+    [SerializeField]
+    private string Direction;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -13,6 +15,21 @@ public class jumpPad : MonoBehaviour
         {
             JumpPadSFX.Play();
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+        }
+
+        if (collision.gameObject.CompareTag("Box"))
+        {
+            JumpPadSFX.Play();
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * (bounce*0.7f), ForceMode2D.Impulse);
+
+            if (Direction == "R")
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * (bounce/6), ForceMode2D.Impulse);
+            }
+            if (Direction == "L")
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce((-Vector2.right) * (bounce/6), ForceMode2D.Impulse);
+            }
         }
     }
 }
