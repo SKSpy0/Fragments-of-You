38ddec165845ee888a36ca30db54945f7fade2d9
Yuhao <hello@chenyuhao.me>
2022-05-02 16:08:32 -0700

    Numerous improvement to zone 1: - New tile - Sunshine Wobblly effect - Spread sunshine effect over z1m1 - Give lock on animation a point light for better visibility

---
+++ Fragments Of You/Assets/Scripts/LightScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    public float startRange = 1.0f;
+    public float endRange = 24f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var oscilationRange = (endRange - startRange) / 2;
+        var oscilationOffset = oscilationRange + startRange;
+        var result = oscilationOffset + Mathf.Sin(Time.time) * oscilationRange;
+        transform.rotation = Quaternion.Euler(Vector3.forward * result);
+    }
+}
