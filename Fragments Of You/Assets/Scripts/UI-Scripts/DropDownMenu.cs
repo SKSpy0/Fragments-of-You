dd7fb5c47825366683e5185dbbb4fa9d8545d27d
hmedina3 <hmedina3@ucsc.edu>
2022-05-06 08:33:24 -0700

    UI update - acesssibility options. - H.

---
+++ Fragments Of You/Assets/Scripts/UI-Scripts/DropDownMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDownMenu : MonoBehaviour
+{
+    //Colorblind colorblindScript;
+
+    public void HandleInputData(int value)
+    {
+        if(value == 0){
+           // new GUIContent("Normal Vision"),
+           // bind the 'Type' parameter of the Colorblind script to dropdown in GUI
+          //  colorblindScript.Type = value;
+        }
+        if(value == 1){
+          // new GUIContent("Protanopia"),
+          // bind the 'Type' parameter of the Colorblind script to dropdown in GUI
+         // colorblindScript.Type = value;
+        }
+        if(value == 2){
+         // new GUIContent("Deuteranopia"),
+           // bind the 'Type' parameter of the Colorblind script to dropdown in GUI
+         // colorblindScript.Type = value;
+        }
+        if(value == 3){
+          // new GUIContent("Tritanopia")
+          // bind the 'Type' parameter of the Colorblind script to dropdown in GUI
+        //  colorblindScript.Type = value;
+        }
+    }
+
+}
