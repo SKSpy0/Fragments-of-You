e892c31471730550907f024161deab74afb1155e
hmedina3 <hmedina3@ucsc.edu>
2022-05-06 02:45:04 -0700

    Improvements and changes made to gameplay: 1) Mega Laser Prefab created 2) Rock destroyers placed on top of spikes in Z2MZ box puzzle section 3) Collectible now follows the player until the survival time ends
    - H.

--- Fragments Of You/Assets/Scripts/Items/Collectible.cs
+++ Fragments Of You/Assets/Scripts/Items/Collectible.cs
@@ -17,6 +17,10 @@ public class Collectible : MonoBehaviour
     public static bool CollectibleOnDeath;
     bool collected;
 
+    private Transform target;
+    private Vector2 originalPosition;
+    private float collectibleSpeed;
+
    
 
     // Start is called before the first frame update
@@ -26,19 +30,36 @@ public class Collectible : MonoBehaviour
         coroutine = CollectibleCollected(5.0f);
         collected = false;
         CollectibleOnDeath = false;
+        // get original position of collectible and store it to reset collectible
+        originalPosition = this.transform.position;
+        // private Transform originalPosition = this.gameObject.GetComponent<Transform>();
+        // set target to player
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        collectibleSpeed = 5f;
+    }
 
+    void Update(){
+       if(Vector2.Distance(transform.position, target.position) > 1.5 && collected){
+           transform.position = Vector2.MoveTowards(transform.position, target.position, collectibleSpeed * Time.deltaTime);
+       }
+       else if(!collected){
+         this.transform.position = originalPosition;
+       }
     }
 
     IEnumerator CollectibleCollected(float waitTime)
     {
-        // waits five seconds and then play font effect
+       
+          // waits five seconds and then play font effect
           yield return new WaitForSeconds(waitTime);
-
-        /* If player dies within five seconds, reset collectibles position
-        * "CollectibleOnDeath" boolean in PinkMovement.cs will 
-        * change to true if player dies.
+        /*  
+        * If player dies within five seconds, reset collectibles 
+        * position "CollectibleOnDeath" boolean in PinkMovement.cs will 
+        * change to true if player dies. 
         */
            if(CollectibleOnDeath){
+            collected = false;
+            // boolean check
             CollectibleSprite.enabled = true;
             CollectibleCollider.enabled = true;
             TotalCollectibles.collectiblesCollected -= 1;
@@ -59,9 +80,9 @@ public class Collectible : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("Player")){
-
+            // boolean check
+            collected = true;
             Debug.Log("Collectible collected!");
-            CollectibleSprite.enabled = false;
             CollectibleCollider.enabled = false;
             // always set the boolean to false when player collides with collectible
             CollectibleOnDeath = false;
