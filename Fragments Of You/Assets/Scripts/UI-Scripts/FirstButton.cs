564421344c04f81300d9982e12acdf8447b5a131
Yufeng Xie <yxie54@ucsc.edu>
2022-05-08 01:25:51 +0800

    Font Change and Refine the Main Menu

---
+++ Fragments Of You/Assets/Scripts/UI-Scripts/FirstButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstButton : MonoBehaviour
+{
+    Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.Select();
+    }
+    
+    public void selectAgain()
+    {
+        button.Select();
+    }
+}
