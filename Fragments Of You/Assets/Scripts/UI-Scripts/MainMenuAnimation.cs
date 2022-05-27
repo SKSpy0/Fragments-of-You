913ce81782182a7c440b09d4455b7d055d66351e
Yufeng Xie <yxie54@ucsc.edu>
2022-05-27 21:16:19 +0800

    New Option Menu for Main Menu

--- Fragments Of You/Assets/Scripts/UI-Scripts/MainMenuAnimation.cs
+++ Fragments Of You/Assets/Scripts/UI-Scripts/MainMenuAnimation.cs
@@ -10,6 +10,7 @@ public class MainMenuAnimation : MonoBehaviour
     public Animator playButtonFade;
     public Animator moreButtonFade;
     public Animator volumnMenuFade;
+    public Animator volumeMenuRealFade;
     public Animator accessbilityMenuFade;
     public Animator creditMenuFade;
     public Animator quitMenuFade;
@@ -17,10 +18,23 @@ public class MainMenuAnimation : MonoBehaviour
     public Animator DarkTransition;
     public GameObject clickToStartObj;
     public GameObject playButton;
-    public GameObject optionButton;
     public GameObject quitButton;
     public GameObject titleText;
 
+    public GameObject myEventSystem;
+
+    bool clicked = false;
+
+    void Start()
+    {
+        myEventSystem = GameObject.Find("EventSystem");
+    }
+
+    public void unSelectButtons()
+    {
+        clicked = false;
+        myEventSystem .GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
+    }
 
     public void ClickToStart()
     {
@@ -32,14 +46,55 @@ public class MainMenuAnimation : MonoBehaviour
         volumnMenuFade.SetBool("Enter", true);
     }
 
+    public void ButtonSelect(GameObject button)
+    {
+        button.GetComponent<Animator>().SetBool("Enter", true);
+    }
+
+    public void ButtonTextSelect(GameObject buttonText)
+    {
+        buttonText.GetComponent<Animator>().SetBool("Enter", true);
+    }
+
+    public void ButtonDeSelect(GameObject button)
+    {
+        button.GetComponent<Animator>().SetBool("Enter", false);
+    }
+
+    public void ButtonTextDeSelect(GameObject buttonText)
+    {
+        buttonText.GetComponent<Animator>().SetBool("Enter", false);
+    }
+
+    public void VolumeRealTransition(GameObject menu)
+    {
+        StartCoroutine(VolumnRealTransitionWait(menu));
+    }
+
+    public void LeavevVolumnRealTransition(GameObject menu)
+    {
+        if (clicked == false)
+        {
+            StartCoroutine(LeavevVolumnRealTransitionWait(menu));
+        }
+    }
+
     public void LeavevVolumnTransition()
     {
         interfaceFade.SetBool("Enter", true);
     }
 
-    public void AccessbilityTransition()
+    public void AccessbilityTransition(GameObject menu)
     {
-        accessbilityMenuFade.SetBool("Enter", true);
+        StartCoroutine(AccessbilityTransitionWait(menu));
+    }
+
+    public void LeaveAccessbilityTransition(GameObject menu)
+    {
+        if (clicked)
+        {
+            StartCoroutine(LeavevAccessbilityTransitionWait(menu));
+        }
     }
 
     public void CreditsTransition()
@@ -75,11 +130,46 @@ public class MainMenuAnimation : MonoBehaviour
         yield return new WaitForSeconds(transitionTime);
         clickToStartObj.SetActive(false);
         playButton.SetActive(true);
-        optionButton.SetActive(true);
         quitButton.SetActive(true);
 
         yield return new WaitForSeconds(transitionTime);
         interfaceFade.SetBool("Enter", true);
+    }
+
+    IEnumerator VolumnRealTransitionWait(GameObject menu)
+    {
+        yield return new WaitForSeconds(0.5f);
+        menu.SetActive(true);
+        volumeMenuRealFade.SetBool("Enter1", true);
+    }
+
+    IEnumerator LeavevVolumnRealTransitionWait(GameObject menu)
+    {
+        volumeMenuRealFade.SetBool("Enter1", false);
+
+        yield return new WaitForSeconds(0.5f);
+
+        accessbilityMenuFade.SetBool("Enter", true);
+        menu.SetActive(false);
+        clicked = true;
+    }
+
+    IEnumerator AccessbilityTransitionWait(GameObject menu)
+    {
+        yield return new WaitForSeconds(0.5f);
+        menu.SetActive(true);
+        accessbilityMenuFade.SetBool("Enter", true);
+    }
+
+    IEnumerator LeavevAccessbilityTransitionWait(GameObject menu)
+    {
+        accessbilityMenuFade.SetBool("Enter", false);
+
+        yield return new WaitForSeconds(0.5f);
+
+        volumeMenuRealFade.SetBool("Enter1", true);
+        menu.SetActive(false);
+        clicked = false;
     }
 
     IEnumerator CreditsTransitionWait()
