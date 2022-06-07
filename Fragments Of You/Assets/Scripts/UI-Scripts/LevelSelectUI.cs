199ecfe33395a9b09856aa944db1f98239db2d9d
Yufeng Xie <yxie54@ucsc.edu>
2022-06-07 21:47:32 +0800

    Move Pink to current level position in levelmap

--- Fragments Of You/Assets/Scripts/UI-Scripts/LevelSelectUI.cs
+++ Fragments Of You/Assets/Scripts/UI-Scripts/LevelSelectUI.cs
@@ -12,8 +12,79 @@ public class LevelSelectUI : MonoBehaviour
     public bool waitIsOver = true;
 
     void Start()
-    {   
+    {
         level_Cap = PlayerPrefs.GetInt("level_reached" + SaveID.saveID);
+        StartCoroutine(placePinkToCurrentLevelTransition());
+        placePinkToCurrentLevel();
+    }
+
+    public void placePinkToCurrentLevel()
+    {
+        if (level_Cap > 0)
+        {
+            level_Count = 1;
+            LevelMapPink.SetInteger("levelCount", level_Count);
+            LevelMapPink.SetBool("Arrive1-2", true);
+        }
+        if (level_Cap > 1)
+        {
+            level_Count = 2;
+            LevelMapPink.SetInteger("levelCount", level_Count);
+            LevelMapPink.SetBool("Arrive1-3", true);
+        }
+        if (level_Cap > 2)
+        {
+            level_Count = 3;
+            LevelMapPink.SetInteger("levelCount", level_Count);
+            LevelMapPink.SetBool("Arrive2-1", true);
+        }
+        if (level_Cap > 3)
+        {
+            level_Count = 4;
+            LevelMapPink.SetInteger("levelCount", level_Count);
+            LevelMapPink.SetBool("Arrive2-2", true);
+        }
+        if (level_Cap > 4)
+        {
+            level_Count = 5;
+            LevelMapPink.SetInteger("levelCount", level_Count);
+            LevelMapPink.SetBool("Arrive3", true);
+        }
+    }
+
+    IEnumerator placePinkToCurrentLevelTransition()
+    {
+        if (level_Cap == 1)
+        {
+            LevelMapPink.SetBool("Arrived1-2", true);
+            yield return new WaitForSeconds(0.1f);
+            LevelMapPink.SetBool("Arrived1-2", false);
+        }
+        if (level_Cap == 2)
+        {
+            LevelMapPink.SetBool("Arrived1-3", true);
+            yield return new WaitForSeconds(0.1f);
+            LevelMapPink.SetBool("Arrived1-3", false);
+        }
+        if (level_Cap == 3)
+        {
+            LevelMapPink.SetBool("Arrived2-1", true);
+            yield return new WaitForSeconds(0.1f);
+            LevelMapPink.SetBool("Arrived2-1", false);
+        }
+        if (level_Cap == 4)
+        {
+            LevelMapPink.SetBool("Arrived2-2", true);
+            yield return new WaitForSeconds(0.1f);
+            LevelMapPink.SetBool("Arrived2-2", false);
+        }
+        if (level_Cap == 5)
+        {
+            LevelMapPink.SetBool("Arrived3", true);
+            yield return new WaitForSeconds(0.1f);
+            LevelMapPink.SetBool("Arrived3", false);
+        }
+        yield return new WaitForSeconds(0f);
     }
 
     public void moveFoward()
